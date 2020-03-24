8c9566822763b778032b7aa0d80e92324a135552
Raphael Kuro <raphaeloper@gmail.com>
2020-03-25 00:21:45 +0200

    Frame loads after calculation.

--- src/com/company/Draw.java
+++ src/com/company/Draw.java
@@ -12,13 +12,14 @@ public class Draw extends Canvas {
     private double xLim = 2.5;                           // x = current tested value; xLim = boundary;
     private double yLim = xLim * HEIGHT / WIDTH;         // y = current tested value; yLim = boundary;
     private double centerX = -1, centerY = 0;
-
+    private int[][] screen = new int[WIDTH + 1][HEIGHT + 1];
     private double diff = xLim / WIDTH / 2; // Limit is 0.0005
     private double magSize = WIDTH / (2 * xLim / diff) * 1 / diff;
     private int ORIGIN_X = WIDTH / 2, ORIGIN_Y = HEIGHT / 2;
 
     public Draw() {
         setSize(WIDTH, HEIGHT);
+        calculate();
     }
 
     @Override
@@ -27,7 +28,7 @@ public class Draw extends Canvas {
         arrColor[0] = Color.BLACK;
         arrColor[1] = new Color(100, 0, 100);
         arrColor[21] = Color.WHITE;
-        arrColor[41] = new Color(0, 0, 0);
+        arrColor[41] = new Color(100, 0, 0);
         arrColor[61] = Color.BLACK;
         for (int i = 2; i < 21; i++)
             arrColor[i] = arrColor[i - 1].brighter();
@@ -37,6 +38,16 @@ public class Draw extends Canvas {
             arrColor[i] = arrColor[i - 1].brighter();
 
 
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                g.setColor(arrColor[screen[x][y]]);
+                g.drawLine(x, y, x, y);
+            }
+        }
+
+    }
+
+    public void calculate() {
         Complex z = new Complex(0, 0);
         Complex c = new Complex(0, 0);
         int iterations, graphX, graphY;
@@ -49,12 +60,11 @@ public class Draw extends Canvas {
                     z.square().add(c);
                     iterations++;
                 }
-                g.setColor(arrColor[iterations]);
                 graphX = ORIGIN_X + (int) (x * magSize);
                 graphY = ORIGIN_Y + (int) (y * (-magSize));
-                g.drawLine(graphX, graphY, graphX, graphY);
-
+                screen[graphX][graphY] = iterations;
             }
         }
+
     }
 }
